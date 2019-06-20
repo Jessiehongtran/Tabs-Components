2bdff0deaf7057f2b1acd1764eecc0b483149151
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-20 18:28:46 -0400

    almost done with TabLink class but stuck at how to link Select methods between TabLink and TabItem

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,33 +5,34 @@ class TabLink {
     this.element = element;
     
     // Get the custom data attribute on the Link
-    this.data = this.element.dataset.link;
+    this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    this.itemElement = document.querySelector(`div.tabs-item[data-tab="${this.data}"]`);
     
+    // console.log(this.itemElement)
     // Using the Item element, create a new instance of the TabItem class
-    this.itemClass = new TabItem(this.element);
+    this.itemClass = new TabItem(this.itemElement);
     
-    console.log(this.itemClass);
+    // console.log(this.itemClass)
     // Add a click event listener on this instance, calling the select method on click
-    this.itemClass.addEventListener('click',()=>{
-      this.select
+    this.element.addEventListener('click',()=>{
+    this.select
     })
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach((link)=>{this.element.classList.toggle("tabs-link-selected")})
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classLink.toggle("tabs-link-selected");
     
     // Call the select method on the item associated with this link
-
+    this.itemClass.select()
   }
 }
 
@@ -63,4 +64,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll(".tabs-link").forEach((link)=>{return new TabLink(link) });
+links = document.querySelectorAll(".tabs-link").forEach((link)=>{return new TabLink(link)});
