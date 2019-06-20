d69e3505ffe95788ea1046e91e41a58778134084
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-20 17:50:25 -0400

    done constructor for TabLink class, move on to the select method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,10 +11,13 @@ class TabLink {
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem = new TabItem();
+    this.itemClass = new TabItem(this.element);
     
+    console.log(this.itemClass);
     // Add a click event listener on this instance, calling the select method on click
-
+    this.itemClass.addEventListener('click',()=>{
+      this.select
+    })
   };
 
   select() {
