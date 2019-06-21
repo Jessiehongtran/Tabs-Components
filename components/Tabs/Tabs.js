0912f597d7137bd2d751f2ad1ea7b4c424598507
HONG TRAN THI THU <jessie@HONGs-MacBook-Air-2.local>
2019-06-20 20:29:06 -0400

    got the TabItem work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,13 +8,13 @@ class TabLink {
     this.data = this.element.dataset.tab;
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`div.tabs-item[data-tab="${this.data}"]`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     
     // console.log(this.itemElement)
     // Using the Item element, create a new instance of the TabItem class
     this.itemClass = new TabItem(this.itemElement);
     
-    console.log(this.itemClass)
+    // console.log(this.itemClass)
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click',()=>{
     this.select()
@@ -22,17 +22,18 @@ class TabLink {
   };
 
   select() {
+  
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach((link)=>{this.element.classList.toggle("tabs-link-selected")})
+    links.forEach(link=>{link.classList.remove("tabs-link-selected")})
 
     // Add a class named "tabs-link-selected" to this link
-    this.element.classList.toggle("tabs-link-selected");
+    this.element.classList.add("tabs-link-selected");
     
     // Call the select method on the item associated with this link
-    
+    this.itemClass.select();
   }
 }
 
@@ -46,11 +47,12 @@ class TabItem {
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll(".tabs-item");
 
+
     // Remove the class "tabs-item-selected" from each element
-    items.classList.toggle("tabs-item-selected");
+    items.forEach(item=>{item.classList.remove("tabs-item-selected")});
     
     // Add a class named "tabs-item-selected" to this element
-    items.classList.toggle("tabs-item-selected");
+    items.forEach(item=>{item.classList.add("tabs-item-selected")});
   }
 }
 
